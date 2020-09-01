b9381311cff99552c86a648d68777d5d0612577e
Allan MacLean <allanmaclean@Allans-MBP.fios-router.home>
2020-08-31 21:28:12 -0400

    create desktop sharing js functionality

--- share-state.js
+++ share-state.js
@@ -1,4 +1,4 @@
-var sharebar = document.querySelectorAll(".author-and-share-container");
+var sharebar = document.querySelectorAll(".card-footer");
 
 for(var i = 0; i < sharebar.length; i++) {
   var shareIcon = sharebar[i].querySelector(".icon-container"), iconCurrent = sharebar[i];
